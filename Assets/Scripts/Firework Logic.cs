ef1afa91e915deb15943c369e6a411562b727dbb
Tmin1021 <128274812+Tmin1021@users.noreply.github.com>
2026-01-08 00:27:35 +0700

    Simple FX + Firework Model

---
+++ Assets/Scripts/Firework Logic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworkLogic : MonoBehaviour
+{
+    [SerializeField] GameObject explosionFX;
+    [SerializeField] float explodeHeight;
+    [SerializeField] float speed = 2f;
+    private Transform fireworkTransform;
+    private float initY;
+
+    void Awake()
+    {
+        fireworkTransform = gameObject.GetComponent<Transform>();
+        initY = fireworkTransform.position.y;
+    }
+
+    void Update()
+    {
+        fireworkTransform.position = Vector3.Lerp(fireworkTransform.position, fireworkTransform.position + Vector3.up * explodeHeight, Time.deltaTime * speed);
+        if(fireworkTransform.position.y >= initY + explodeHeight)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionFX, fireworkTransform.position, Quaternion.identity);
+        }
+    }
+}
